bd63974e74cc9e03a659d3f86d2c58f0135beef8
leehomeecky <meecky4chist@gmail.com>
2023-10-25 02:11:26 +0100

    module scafolding

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AuthModule } from './modules/auth/auth/auth.module';
+import { AuthModule } from './modules/auth/auth.module';
+import { TeamModule } from './modules/team/team.module';
+import { FixtureModule } from './modules/fixture/fixture.module';
 
 @Module({
-  imports: [],
+  imports: [AuthModule, TeamModule, FixtureModule],
   controllers: [AppController],
   providers: [AppService],
 })
