7e357fd214a5085990f5f8a752355ed8d5738a1e
leehomeecky <meecky4chist@gmail.com>
2023-10-25 20:31:25 +0100

    schema setup

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,17 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Req, Res } from '@nestjs/common';
 import { AppService } from './app.service';
+import { Request, Response } from 'express';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  async getServerStatus(@Req() req: Request, @Res() resp) {
+    const result = await this.appService.getServerStatus();
+    resp.json({
+      code: 0,
+      message: result,
+    });
   }
 }
--- src/app.service.ts
+++ src/app.service.ts
@@ -2,7 +2,8 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  async getServerStatus(): Promise<string> {
+    const { port } = process.env;
+    return `Servar runing on port ${port || 9800}`;
   }
 }
